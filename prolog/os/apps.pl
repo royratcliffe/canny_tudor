475b8771acf349e10f026f4cc2420965d853e2f3
Roy Ratcliffe <royratcliffe@me.com>
2019-06-13 20:54:31 +0000

    Comment on os_apps

--- prolog/os/apps.pl
+++ prolog/os/apps.pl
@@ -1,12 +1,38 @@
 :- module(os_apps,
-          [   start_app/1,
-              stop_app/1
+          [   start_app/1,              % ?App:compound
+              stop_app/1                % ?App:compound
           ]).
 
 :- meta_predicate
     detach(0, +).
 
-/*
+/** <module> Operation system apps
+ *
+ * What is an app? In an operating-system context, simply something you
+ * can start and stop using a process. It has no standard input, and
+ * typically none or minimal standard output and error. Processes have
+ * four distinct inputs: a path specification, a list of arguments,
+ * possibly some execution options along with some optional encoding and
+ * other run-time related options. Call this the application's
+ * configuration. This may seem like an unusual definition of "app" and
+ * maybe too technical, but English limits the alternatives: process, no
+ * because that means something that loads an app; program, no because
+ * that generally refers the app's image including its resources.
+ *
+ * For all the above reasons, the special multi-file predicate
+ * os:app_property/2 supplies an app's configuration deterministically
+ * using four sub-terms for the second argument, as follows.
+ *
+ * - os:app_property(App, path(Path))
+ * - os:app_property(App, argument(Argument))
+ * - os:app_property(App, option(Option))
+ *
+ * Two things to note about these predicate calls; (1) App is a compound
+ * describing the app and its configuration information; (2) the second
+ * argument collates arguments and options non-deterministically.
+ * Predicate start_program/1 finds all the argument- and
+ * option-solutions in the order defined.
+ *
  * There is an important distinction between programs and processes.
  * These predicates use processes to launch programs. Programs in this
  * sense is more akin to an application, where one application has
