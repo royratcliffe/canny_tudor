63f779e8d0349065193c4ea32693f096653b961e
Roy Ratcliffe <royratcliffe@me.com>
2019-08-11 19:23:03 +0000

    Experimental wsdl_qualified_name/3

--- prolog/ws/dl.pl
+++ prolog/ws/dl.pl
@@ -1,4 +1,4 @@
-:- module(ws_dl, [element_xmlns/2]).
+:- module(ws_dl, [element_xmlns/2, wsdl_qualified_name/3]).
 
 ns(wsdl, 'http://schemas.xmlsoap.org/wsdl/').
 
@@ -17,3 +17,35 @@ element_xmlns(element(_, Attributes, _), Name=URI) :-
 element_xmlns(element(_, _, Content), Name=URI) :-
     member(Element, Content),
     element_xmlns(Element, Name=URI).
+
+%!  wsdl_qualified_name(+WSDL:compound, ?QName:atom, ?URILocal:compound)
+%   is nondet.
+%
+%   Unfies QName withits URI and Local part. Atom becomes compound, or
+%   vice versa. WSDL definitions provide the context. Fails if the
+%   context cannot match the Name. Relates a qualified name to a
+%   URI:Local compound. Only Local needs to be ground; Name or URI, or
+%   both, can be unbound.
+%
+%   QName is an atom. It is *not* a compound, even though it may look
+%   like one. QName formats as an atom comprising a colon-delimited
+%   Name and Local, where Name refers to a XML namespace qualifier and
+%   Local refers to its arbitrary local part. There is no Name-based
+%   unification. Unify Name=URI using element_xmlns/2 to narrow search
+%   scope to some specific Name.
+%
+%   Tempting to prepend a second optimised clause for ground atomic
+%   QName arguments.
+%
+%       wsdl_qualified_name(WSDL, QName, URI:Local) :-
+%           atom(QName),
+%           !,
+%           sub_atom(QName, Before, _, After, :),
+%           sub_atom(QName, 0, Before, _, Name),
+%           element_xmlns(WSDL, Name=URI),
+%           sub_atom(QName, _, After, 0, Local).
+
+wsdl_qualified_name(WSDL, QName, URI:Local) :-
+    element_xmlns(WSDL, Name=URI),
+    atom_concat(Name, :, Name_),
+    atom_concat(Name_, Local, QName).
