a4a2c704c816f805505ee6f5cb386effa90744dd
Roy Ratcliffe <royratcliffe@me.com>
2019-10-14 16:39:07 +0000

    Optimise determinism using predicate indexer (for_situation)

--- prolog/canny/situations.pl
+++ prolog/canny/situations.pl
@@ -5,17 +5,68 @@
     situation_property(:, ?).
 
 :- multifile
-    canny:situation/2,
-    canny:situation_property/2.
+    canny:for_situation/2,
+    canny:property_for_situation/2.
 
 :- use_module(library(random/temporary)).
 
 :- dynamic situation_module/2.
 
-situation(Situation, Term) :-
-    canny:situation(Situation, Term).
+%!  situation(?Situation:any, ?Term) is nondet.
+%
+%   Apply Term to Situation, where Term is one of the following.
+%
+%       * module(Module)
+%
+%       Sets  up  Situation  using  Module.    Establishes  the  dynamic
+%       predicate options for the temporary   situation  module used for
+%       persisting situation Now-At and Was-When tuples.
+%
+%       An important side effect occurs for  ground Situation terms. The
+%       implementation creates the  situation's   temporary  module  and
+%       applies default options  to  its   new  dynamic  predicates. The
+%       module(Module) term unifies with the   newly-created or existing
+%       situation module.
+%
+%       The predicate's determinism collapses   to  semi-determinism for
+%       ground situations. Otherwise with variable Situation components,
+%       the predicate unifies with  all   matching  situations, unifying
+%       with module(Module) non-deterministically.
+%
+%       * now(Now, At)
+%
+%       Makes some Situation become Now for time   index At, at the next
+%       fixation. Effectively schedules a  pending   update  one or more
+%       times; the next situation =fix/0=   fixes  the pending situation
+%       changes at some future point.
+%
+%       Uses canny:for_situation/2 when Situation is   ground,  but uses
+%       canny:property_for_situation/2 otherwise. Asserts  therefore for
+%       multiple situations if Situation comprises variables. You cannot
+%       therefore have non-ground situations.
+%
+%       * fix
+%
+%       Fixating situations does three important  things. First, it adds
+%       new Previous-When pairs to the   situation  history. They become
+%       =was/2= dynamic facts (clauses without  rules). Second, it adds,
+%       replaces or removes the  most   current  Current-When pair. This
+%       allows detection of non-events, e.g.  when something disappears.
+%       Some types of situation might require such event edges. Finally,
+%       fixating broadcasts situation-change messages.
+%
+%       The rule for fixing the  Current-When   pair  goes like this: Is
+%       there a new =now/2=, at least one?   The  latest becomes the new
+%       current.  Any  others  become  Previous-When.  If  there  is  no
+%       =now/2=,  then  the  current    disappears.  Messages  broadcast
+%       accordingly. If there is more than  one =now/2=, only the latest
+%       becomes current. Hence  currently-previously   only  transitions
+%       once in-between fixations.
 
-canny:situation(Situation, module(Module)) :-
+situation(Situation, Term) :-
+    canny:for_situation(Term, Situation).
+
+canny:for_situation(module(Module), Situation) :-
     with_mutex(canny_situations, temporary_module(Situation, Module)).
 
 temporary_module(Situation, Module) :-
@@ -31,7 +82,7 @@ temporary_module(Situation, Module) :-
                 Module:previously/2
             ], []).
 
-canny:situation(Situation, now(Now, At)) :-
+canny:for_situation(now(Now, At), Situation) :-
     ground(Now),
     number(At),
     now(Situation, Now, At).
@@ -39,21 +90,20 @@ canny:situation(Situation, now(Now, At)) :-
 now(Situation, Now, At) :-
     ground(Situation),
     !,
-    canny:situation(Situation, module(Module)),
+    temporary_module(Situation, Module),
     assertz(Module:now(Now, At)).
 now(Situation, Now, At) :-
-    forall(canny:situation(Situation, module(Module)),
-           assertz(Module:now(Now, At))).
+    forall(situation_module(Situation, Module), assertz(Module:now(Now, At))).
 
-canny:situation(Situation, fix) :-
+canny:for_situation(fix, Situation) :-
     situation_module(Situation, Module),
     fix(Situation, Module).
 
 fix(Situation, Module) :-
-    findall(Now0-At0, retract(Module:now(Now0, At0)), Fixes0),
+    findall(now(Now, At), retract(Module:now(Now, At)), Fixes0),
     sort(2, @=<, Fixes0, Fixes),
-    forall(member(Now-At, Fixes), asserta(Module:was(Now, At))),
-    ignore(retract(Module:previously(_, _))),
+    forall(member(now(Was, When), Fixes), asserta(Module:was(Was, When))),
+    ignore(retractall(Module:previously(_, _))),
     fix(Fixes, Situation, Module),
     broadcast(situation(Situation, fix)).
 
@@ -65,7 +115,7 @@ fix([], Situation, Module) :-
 fix([], _, _) :-
     !.
 fix(Fixes, Situation, Module) :-
-    last(Fixes, Now-At),
+    last(Fixes, now(Now, At)),
     fix(Situation, Module, Now, At).
 
 fix(_, Module, Now, _) :-
@@ -83,7 +133,7 @@ fix(Situation, Module, Now, At) :-
     asserta(Module:currently(Now, At)),
     broadcast(situation(Situation, now(Now, At))).
 
-canny:situation(Situation, retract(When)) :-
+canny:for_situation(retract(When), Situation) :-
     situation_module(Situation, Module),
     retract(Situation, Module, When).
 
@@ -93,7 +143,7 @@ retract(Situation, Module, When0) :-
            ), once(retract(Module:was(Was, When)))),
     broadcast(situation(Situation, retract(When0))).
 
-canny:situation(Situation, retract(When, Delay)) :-
+canny:for_situation(retract(When, Delay), Situation) :-
     number(Delay),
     situation_module(Situation, Module),
     retract(Situation, Module, When, Delay).
@@ -107,23 +157,43 @@ retract(Situation, Module, When, Delay) :-
     When0 is When - Delay,
     retract(Situation, Module, When0).
 
-canny:situation(Situation, listing) :-
+canny:for_situation(listing, Situation) :-
     situation_module(Situation, Module),
     listing(Module:_).
 
-situation_property(Situation, Property) :-
-    canny:situation_property(Situation, Property).
+%!  situation_property(?Situation:any, ?Property) is nondet.
+%
+%   Property of Situation.
+%
+%       * module(Module)
+%
+%       Marries situation terms with universally-unique modules, one for
+%       one. All dynamic situations link a situation term with a module.
+%       This design addresses performance. Retracts   take  a long time,
+%       relatively, especially for dynamic  predicates   with  very many
+%       clauses; upwards of 10,000 clauses for   example.  Note, you can
+%       never delete the  situation-module  association,   but  you  can
+%       retract all the dynamic clauses belonging to a situation.
+%
+%       * defined
+%
+%       Situation is defined whenever a  unique situation module already
+%       exists for the given Situation. Amounts   to  the same as asking
+%       for module(_) property.
 
-canny:situation_property(Situation, module(Module)) :-
+situation_property(Situation, Property) :-
+    canny:property_for_situation(Property, Situation).
+
+canny:property_for_situation(module(Module), Situation) :-
     situation_module(Situation, Module).
-canny:situation_property(Situation, defined) :-
+canny:property_for_situation(defined, Situation) :-
     situation_module(Situation, _).
-canny:situation_property(Situation, currently(Current, When)) :-
+canny:property_for_situation(currently(Current, When), Situation) :-
     situation_module(Situation, Module),
     once(Module:currently(Current, When)).
-canny:situation_property(Situation, previously(Previous, When)) :-
+canny:property_for_situation(previously(Previous, When), Situation) :-
     situation_module(Situation, Module),
     once(Module:previously(Previous, When)).
-canny:situation_property(Situation, history(History)) :-
+canny:property_for_situation(history(History), Situation) :-
     situation_module(Situation, Module),
     findall(was(Was, When), Module:was(Was, When), History).
