f4aa6a23c6124dcf466c8dec4da0db7600cce74d
Roy Ratcliffe <royratcliffe@me.com>
2019-11-11 18:06:53 +0000

    Comments

--- prolog/linear/algebra.pl
+++ prolog/linear/algebra.pl
@@ -1,15 +1,15 @@
 :- module(linear_algebra,
-          [   matrix_dimensions/3,
+          [   matrix_dimensions/3,      % ?Matrix, ?Rows, ?Columns
               matrix_identity/2,        % +Order, -Matrix
               matrix_transpose/2,       % ?Matrix0, ?Matrix
               matrix_multiply_vector/3,
               matrix_multiply/3,
-              matrix_rotation/2,
+              matrix_rotation/2,        % ?Theta, ?Matrix
 
               vector_dimension/2,
               vector_distance/2,        % ?V, ?Distance
               vector_distance/3,        % ?U, ?V, ?Distance
-              vector_translate/3,
+              vector_translate/3,       % ?U, ?V, ?W
               vector_multiply/3,
               vector_scale/3,           % ?Scalar, ?U, ?V
               vector_heading/2,         % ?V, ?Heading
@@ -23,8 +23,62 @@
 
 /** <module> Linear algebra
  *
+ * "The  introduction  of  numbers  as   coordinates    is   an  act  of
+ * violence."--Hermann Weyl, 1885-1955.
+ *
+ * Vectors are just lists of numbers, or scalars. These scalars apply to
+ * arbitrary abstract dimensions. For example,  a two-dimensional vector
+ * [1, 2] applies two scalars, 1  and   2,  to dimensional units _i_ and
+ * _j_; known as the basis vectors for the coordinate system.
+ *
+ * Is it possible, advisable, sensible  to   describe  vector and matrix
+ * operations using Constraint Logic Programming  (CLP) techniques? That
+ * is, since vectors and matrices  are   basically  columns  and rows of
+ * real-numeric  scalars,  their   operators    amount   to  constrained
+ * relationships between real numbers and hence  open to the application
+ * of CLP over reals.  The  simple   answer  is  yes, the linear_algebra
+ * predicates  let  you  express  vector   operators  using  real-number
+ * constraints.
+ *
+ * Constraint logic adds some important   features to vector operations.
+ * Suppose for instance that you have a  simple addition of two vectors,
+ * a vector translation of U+V=W. Add  U   to  V giving W. The following
+ * statements all hold true. Note that the CLP-based translation unifies
+ * correctly when W is unknown but also when   U  or V is unknown. Given
+ * any two, you can ask for the missing vector.
+ *
+ *     ?- vector_translate([1, 1], [2, 2], W).
+ *     W = [3.0, 3.0] ;
+ *     false.
+ *     ?- vector_translate([1, 1], V, [3, 3]).
+ *     V = [2.0, 2.0] ;
+ *     false.
+ *     ?- vector_translate(U, [2, 2], [3, 3]).
+ *     U = [1.0, 1.0] ;
+ *     false.
+ *
+ * Note also that  the  predicate   answers  non-deterministically  with
+ * back-tracking until no alternative answer  exists. This presumes that
+ * alternatives could exist at  least  in   theory  if  not in practice.
+ * Trailing choice-points remain unless you cut them.
+ *
  * @author Roy Ratcliffe <royratcliffe@me.com>
  */
+
+%!  matrix_dimensions(?Matrix:list(list(number)), ?Rows:nonneg,
+%!  ?Columns:nonneg) is semidet.
+%
+%   Dimensions of Matrix where dimensions are Rows and Columns.
+%
+%   A matrix of M rows and N columns  is an M-by-N matrix. A matrix with
+%   a single row is a row vector; one   with a single column is a column
+%   vector. Because the linear_algebra module   uses  lists to represent
+%   vectors and matrices, you need  never   distinguish  between row and
+%   column vectors.
+%
+%   Boundary cases exist. The dimensions of   an  empty matrix [] equals
+%   [0, _] rather than [0, 0].  And   this  works in reverse; the matrix
+%   unifying with dimensions [0, _] equals [].
 
 matrix_dimensions(Matrix, Rows, Columns) :-
     length(Matrix, Rows),
@@ -82,6 +136,14 @@ matrix_multiply_vector(M, U, V) :-
 matrix_multiply(A, B, M) :-
     maplist(matrix_multiply_vector(B), A, M).
 
+%!  matrix_rotation(?Theta:number, ?Matrix:list(list(number))) is
+%!  nondet.
+%
+%   The constructed matrix applies  to  column   vectors  [X,  Y]  where
+%   positive Theta rotates  X  and   Y  anticlockwise;  negative rotates
+%   clockwise. Transpose the rotation matrix  to   reverse  the angle of
+%   rotation; positive for clockwise, negative anticlockwise.
+
 matrix_rotation(Theta, [[A, B], [C, A]]) :-
     {A =:= cos(Theta), B =:= sin(Theta), C =:= -B}.
 
@@ -107,6 +169,12 @@ vector_distance(V, Distance) :-
 vector_distance(U, V, Distance) :-
     vector_translate(U, W, V),
     vector_distance(W, Distance).
+
+%!  vector_translate(?U, ?V, ?W) is nondet.
+%
+%   Translation works forwards and  backwards.   Since  U+V=W it follows
+%   that U=W-V and also V=W-U. So for  unbound U, the vector becomes W-V
+%   and similarly for V.
 
 vector_translate([], [], []).
 vector_translate([X|U], [Y|V], [Z|W]) :-
