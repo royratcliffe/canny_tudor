408c705515fdd5ec17c4e3d49818c33e7419dfef
Roy Ratcliffe <royratcliffe@me.com>
2020-01-16 06:07:23 +0000

    Rename to permute_sum_of_int/2

--- prolog/canny/maths.pl
+++ prolog/canny/maths.pl
@@ -5,7 +5,7 @@
               epsilon_equal/2,
               epsilon_equal/3,
 
-              permute_sum/2,
+              permute_sum_of_int/2,
               permute_list_to_grid/2
           ]).
 
@@ -42,18 +42,18 @@ epsilon_equal(X, Y) :- epsilon_equal(1, X, Y).
 
 epsilon_equal(Epsilons, X, Y) :- Epsilons * epsilon >= abs(X - Y).
 
-%!  permute_sum(+N:nonneg, -Integers:list(integer)) is nondet.
+%!  permute_sum_of_int(+N:nonneg, -Integers:list(integer)) is nondet.
 %
 %   Permute sum. Non-deterministically finds all combinations of integer
 %   sums between 1 and N. Assumes  that   0<=N.  The  number of possible
 %   permutations amounts to 2-to-the-power of  N-1;   for  N=3 there are
 %   four as follows: 1+1+1+1, 1+2, 2+1 and 3.
 
-permute_sum(0, []) :- !.
-permute_sum(N, [H|T]) :-
+permute_sum_of_int(0, []) :- !.
+permute_sum_of_int(N, [H|T]) :-
     between(1, N, H),
     N0 is N - H,
-    permute_sum(N0, T).
+    permute_sum_of_int(N0, T).
 
 %!  permute_list_to_grid(+List0:list, -List:list(list)) is nondet.
 %
@@ -68,7 +68,7 @@ permute_sum(N, [H|T]) :-
 
 permute_list_to_grid(List0, List) :-
     length(List0, N),
-    permute_sum(N, Lengths),
+    permute_sum_of_int(N, Lengths),
     permute_list_to_grid_(List0, Lengths, List).
 
 permute_list_to_grid_([], [], []) :- !.
