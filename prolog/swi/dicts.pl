c8945c75d47b985b06f6c3920d301cc7f3772a06
Roy Ratcliffe <royratcliffe@me.com>
2020-01-21 21:21:10 +0000

    Implement dict_tag/2

--- prolog/swi/dicts.pl
+++ prolog/swi/dicts.pl
@@ -8,7 +8,9 @@
               dict_member/2,            % ?Dict:dict, ?Member
 
               % ?Tag, ?Template, :Goal, -Dicts:list(dict)
-              findall_dict/4
+              findall_dict/4,
+
+              dict_tag/2                % +Dict, ?Tag
           ]).
 
 :- use_module(compounds).
@@ -188,3 +190,31 @@ findall_dict(Tag, Template, Goal, Dicts) :-
 
 findall_dict_(Tag, Dict, Dict) :-
     is_dict(Dict, Tag).
+
+%!  dict_tag(+Dict, ?Tag) is semidet.
+%
+%   Tags Dict with Tag if currently   untagged.  Fails if already tagged
+%   but not matching Tag, just like is_dict/2   with a ground tag. Never
+%   mutates ground tags as  a  result.   Additionally  Tags  all  nested
+%   sub-dictionaries using Tag and the   sub-key for the sub-dictionary.
+%   An underscore delimiter concatenates the tag and key.
+%
+%   The implementation uses atomic concatenation to   merge  Tag and the
+%   dictionary  sub-keys.  Note  that   atomic_list_concat/3  works  for
+%   non-atomic keys, including numbers and   strings.  Does not traverse
+%   sub-lists. Ignores sub-dictionaries where a   dictionary  value is a
+%   list containing dictionaries. Perhaps future versions will.
+
+dict_tag(Dict, Tag) :-
+    is_dict(Dict, Tag),
+    dict_pairs(Dict, Tag, Pairs),
+    pairs_tag(Pairs, Tag).
+
+pairs_tag([], _).
+pairs_tag([Key-Value|T], Tag) :-
+    (   is_dict(Value)
+    ->  atomic_list_concat([Tag, Key], '_', Tag_),
+        dict_tag(Value, Tag_)
+    ;   true
+    ),
+    pairs_tag(T, Tag).
--- prolog/swi/dicts.plt
+++ prolog/swi/dicts.plt
@@ -20,4 +20,7 @@ test(dict_member, [true(A==a{b:c{d:e{f:1}}}), nondet]) :-
 test(dict_member, [true(A-B==a^b/c^d/e^f/g^h/i^j-999), nondet]) :-
     dict_member(a{b:c{d:e{f:g{h:i{j:999}}}}}, A-B).
 
+test(dict_tag, [true(A-B==tag-tag_sub)]) :-
+    dict_tag(A{sub:B{}}, tag).
+
 :- end_tests(swi_dicts).
