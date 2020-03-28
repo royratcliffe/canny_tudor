f0d7c7877230fe82c751fbde0dd34a5e99ace653
Roy Ratcliffe <royratcliffe@me.com>
2020-03-28 13:18:28 +0000

    Situation debugging: implement print_situation_history_lengths/0

--- prolog/canny/situations_debugging.pl
+++ prolog/canny/situations_debugging.pl
@@ -1,4 +1,4 @@
-:- module(canny_situations_debugging, []).
+:- module(canny_situations_debugging, [print_situation_history_lengths/0]).
 
 listen :-
     unlisten,
@@ -18,3 +18,16 @@ situation(Arguments) :-
     debug(situation, '~q', [Term]).
 
 :- initialization listen.
+
+:- use_module(library(print/table)).
+
+%!  print_situation_history_lengths is det.
+
+print_situation_history_lengths :-
+    findall((Module:Situation)-Length,
+            (   situation_property(Module:Situation, history(History)),
+                length(History, Length)
+            ), Situations),
+    convlist([First-Second, First-Second]>>(Second > 1), Situations, Filtered),
+    sort(2, @>=, Filtered, Sorted),
+    print_table(member(_-_, Sorted)).
