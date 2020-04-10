35dd70389f34901ce3d5d3550a0ff5b959f49a5e
Roy Ratcliffe <royratcliffe@me.com>
2020-04-10 10:06:31 +0000

    Version 0.7.0

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(canny_tudor).
-version('0.6.1').
+version('0.7.0').
 title('Canny bag o'' Tudor').
 author('Roy Ratcliffe', 'royratcliffe@me.com').
 packager('Roy Ratcliffe', 'royratcliffe@me.com').
