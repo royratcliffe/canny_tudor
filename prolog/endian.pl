ed8598fd8ba1c70521b0b014516016ef89bc6c87
Roy Ratcliffe <royratcliffe@me.com>
2020-08-06 16:40:04 +0000

    White space adjustments; documentation

--- prolog/endian.pl
+++ prolog/endian.pl
@@ -1,6 +1,12 @@
 :- module(endian, [byte//1, big_endian//2, little_endian//2]).
 
+/** <module> Big- and little-endian grammars
+ *
+ */
+
 :- use_module(library(clpfd)).
+
+%!  byte(?Byte:integer)// is semidet.
 
 byte(Byte) -->
     { Byte #= Octet /\ 0xff,
@@ -11,32 +17,43 @@ byte(Byte) -->
 %!  big_endian(?Width:integer, ?Word:integer)// is semidet.
 
 big_endian(16, Word16) -->
-    { high_low(16, High, Low, Word16) },
+    { high_low(16, High, Low, Word16)
+    },
     byte(High),
     byte(Low).
 big_endian(32, Word32) -->
-    { high_low(32, High, Low, Word32) },
+    { high_low(32, High, Low, Word32)
+    },
     big_endian(16, High),
     big_endian(16, Low).
 big_endian(64, Word64) -->
-    { high_low(64, High, Low, Word64) },
+    { high_low(64, High, Low, Word64)
+    },
     big_endian(32, High),
     big_endian(32, Low).
 
 %!  little_endian(?Width:integer, ?Word:integer)// is semidet.
 
 little_endian(16, Word16) -->
-    { high_low(16, High, Low, Word16) },
+    { high_low(16, High, Low, Word16)
+    },
     byte(Low),
     byte(High).
 little_endian(32, Word32) -->
-    { high_low(32, High, Low, Word32) },
+    { high_low(32, High, Low, Word32)
+    },
     little_endian(16, Low),
     little_endian(16, High).
 little_endian(64, Word64) -->
-    { high_low(64, High, Low, Word64) },
+    { high_low(64, High, Low, Word64)
+    },
     little_endian(32, Low),
     little_endian(32, High).
+
+%!  high_low(?Width:integer,
+%!           ?High:integer,
+%!           ?Low:integer,
+%!           ?Word:integer) is semidet.
 
 high_low(16, High, Low, Word16) :-
     Low #= Word16 /\ 0xff,
