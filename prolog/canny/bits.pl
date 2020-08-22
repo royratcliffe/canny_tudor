48de646084c3025982bf2958c480f11f2c57cc93
Roy Ratcliffe <royratcliffe@me.com>
2020-08-22 05:48:49 +0000

    Refactor bits/5

--- prolog/canny/bits.pl
+++ prolog/canny/bits.pl
@@ -1,22 +1,15 @@
-:- module(canny_bits, [bits/5, bits/4]).
+:- module(canny_bits, [bits/5, bits/4, bits/3]).
 
 :- use_module(library(clpfd)).
 
-bits_(Shift, Width, Word0, Word, Remainder) :-
-    Word0_ #= Word0 >> Shift,
-    Word_ #= Word >> Shift,
-    bits(Width, Word0_, Word_, Remainder_),
-    Remainder #= Remainder_ << Shift.
-
-bits(Width, Word0, Word, Rest) :-
-    Mask #= 1 << Width - 1,
-    Word #= Word0 /\ Mask,
-    Rest #= Word0 xor Word,
-    Word0 #= Word /\ Mask \/ Rest.
-
-bits(Shift, Width, Word0, Word, Rest) :-
+bits(Shift, Width, Word, Bits, Rest) :-
     Mask #= (1 << Width - 1) << Shift,
-    Word_ #= Word0 /\ Mask,
-    Rest #= Word0 xor Word_,
-    Word0 #= Word_ /\ Mask \/ Rest,
-    Word #= Word_ >> Shift.
+    Bits0 #= Word /\ Mask,
+    Rest #= Word xor Bits0,
+    Word #= Bits0 /\ Mask \/ Rest,
+    Bits #= Bits0 >> Shift,
+    Bits0 #= Bits << Shift.
+
+bits(Shift-Width, Word, Bits, Rest) :- bits(Shift, Width, Word, Bits, Rest).
+
+bits(Shift-Width, Word, Bits) :- bits(Shift, Width, Word, Bits, _Rest).
