0b8b6bcfdb1f718b11f7bef0f8814b59378f6e0a
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-08-28 17:22:12 +0100

    Add ../prolog to library search path

--- man/doc.pl
+++ man/doc.pl
@@ -1,3 +1,9 @@
+:- initialization doc.
+
+:- multifile user:file_search_path/2.
+
+user:file_search_path(library, '../prolog').
+
 doc :-
     bagof(File, doc(File), Files),
     doc_latex(Files, 'doc.tex', [stand_alone(false), section_level(chapter)]),
