fa30b6597133c6f509f12afd34479f5c9c0759eb
Roy Ratcliffe <royratcliffe@me.com>
2020-08-29 11:21:24 +0000

    Use M:Head

--- prolog/canny/payloads.pl
+++ prolog/canny/payloads.pl
@@ -58,23 +58,23 @@ apply_to(Apply, _M:_To) :- var(Apply), !, fail.
 apply_to(reset, M:To) :- !, retractall(payload(M:To, _)).
 apply_to(push, M:To) :-
     !,
-    of(M:To, [new, Payload], Term),
-    M:Term,
+    of(M:To, [new, Payload], Head),
+    M:Head,
     asserta(payload(M:To, Payload)).
 apply_to(peek(Payload), M:To) :- !, payload(M:To, Payload).
 apply_to(pop(Payload), M:To) :- !, retract(payload(M:To, Payload)).
 apply_to(Applies, M:To) :-
     is_list(Applies),
     !,
-    to(M:To, [_, _, _], Term),
-    functor(Term, Name, 3),
+    to(M:To, [_, _, _], Head),
+    functor(Head, Name, 3),
     once(apply_to(peek(Payload0), M:To)),
     foldl(M:Name, Applies, Payload0, Payload),
     once(apply_to(pop(_Payload0), M:To)),
     asserta(payload(M:To, Payload)).
-apply_to(Apply, M:To) :- to(M:To, [Apply], Term), M:Term.
+apply_to(Apply, M:To) :- to(M:To, [Apply], Head), M:Head.
 
-to(M:To, Args, Term) :- visible(apply_to_, To, Args, M:Term).
+to(M:To, Args, Head) :- visible(apply_to_, To, Args, M:Head).
 
 %!  property_of(+Property, :Of) is nondet.
 
@@ -82,8 +82,8 @@ property_of(Property, _M:_Of) :- var(Property), !, fail.
 property_of(top(Property), M:Of) :-
     !,
     once(apply_to(peek(Payload), M:Of)),
-    of(M:Of, [Property, Payload], Term),
-    M:Term.
-property_of(Property, M:Of) :- of(M:Of, [Property], Term), M:Term.
+    of(M:Of, [Property, Payload], Head),
+    M:Head.
+property_of(Property, M:Of) :- of(M:Of, [Property], Head), M:Head.
 
-of(M:Of, Args, Term) :- visible(property_of_, Of, Args, M:Term).
+of(M:Of, Args, Head) :- visible(property_of_, Of, Args, M:Head).
