e6d8786b905944871d1662ea24cbbb6954a4f2bd
Roy Ratcliffe <royratcliffe@me.com>
2020-09-12 21:06:58 +0000

    Export and document

--- prolog/canny/files.pl
+++ prolog/canny/files.pl
@@ -1,4 +1,21 @@
-:- module(canny_files, []).
+:- module(canny_files, [absolute_directory/2]).
+
+%!  absolute_directory(+Absolute, -Directory) is nondet.
+%
+%   Finds the directories of Absolute by   walking  up the absolute path
+%   until it reaches the root. Operates on paths only; it does not check
+%   that Absolute actually exists. Absolute can   be a directory or file
+%   path.
+%
+%   Fails if Absolute  is  not  an   absolute  file  name,  according to
+%   is_absolute_file_name/2. Works correctly for Unix and Windows paths.
+%   However, it finally unifies with the drive letter under Windows, and
+%   the root directory (/) on Unix.
+%
+%   @arg Absolute specifies an absolute path   name.  On Windows it must
+%   typically include a driver letter, else not absolute in the complete
+%   sense  under  Microsoft  Windows  since  its  file  system  supports
+%   multiple root directories on different mounted drives.
 
 absolute_directory(Absolute, Directory) :-
     is_absolute_file_name_or_root(Absolute),
