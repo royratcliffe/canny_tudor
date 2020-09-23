3637c5f9a036328b57d7061e0830b5d27786f36e
Roy Ratcliffe <royratcliffe@me.com>
2020-09-23 12:56:09 +0000

    Optimise file search path

--- prolog/os/windows.pl
+++ prolog/os/windows.pl
@@ -1,27 +1,40 @@
 :- module(os_windows, []).
 
-/** <module> File searches
+/** <module> Microsoft Windows Operating System
  *
  * By design, the following extensions for Windows avoid underscores
  * in order not to clash with existing standard paths, e.g. =app_path=
  * which Prolog defines by default.
  *
+ *     userprofile
+ *     onedrive
+ *     onedrivecommercial
+ *     onedrivepersonal
+ *     programfiles
+ *     temp
+ *     documents
+ *     savedgames
+ *     appdata
+ *     applocal
+ *     localprograms
+ *
  */
 
 :- multifile user:file_search_path/2.
 
-user:file_search_path(Name, Path) :-
+name_path(Name, Path) :-
     current_prolog_flag(windows, true),
-    name(Name),
     getenv(Name, Path),
     exists_directory(Path).
 
-name(userprofile).
-name(onedrive).
-name(onedrivecommercial).
-name(onedrivepersonal).
-name(programfiles).
-name(temp).
+user:file_search_path(userprofile, Path) :- name_path(userprofile, Path).
+user:file_search_path(onedrive, Path) :- name_path(onedrive, Path).
+user:file_search_path(onedrivecommercial, Path) :-
+    name_path(onedrivecommercial, Path).
+user:file_search_path(onedrivepersonal, Path) :-
+    name_path(onedrivepersonal, Path).
+user:file_search_path(programfiles, Path) :- name_path(programfiles, Path).
+user:file_search_path(temp, Path) :- name_path(temp, Path).
 
 user:file_search_path(documents, userprofile('Documents')).
 user:file_search_path(savedgames, userprofile('Saved Games')).
