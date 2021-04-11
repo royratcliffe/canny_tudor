da4521489a591a98da0c6e6269caf01f0b0e9404
Roy Ratcliffe <royratcliffe@me.com>
2021-04-11 19:07:36 +0000

    More docs

--- prolog/proc/loadavg.pl
+++ prolog/proc/loadavg.pl
@@ -11,6 +11,17 @@
 %   Parses the Linux `/proc/loadavg`  process   pseudo  file.  One space
 %   separates  all  fields  except  the  runnable  processes  and  total
 %   processes, a forward slash separates these two figures.
+%
+%   Load-average statistics comprise: three floating  point numbers, one
+%   integer ratio and one process identifier.
+%
+%       - Load average for last minute
+%       - Load average for last five minutes
+%       - Load average for last 15 minutes
+%       - Number of currently-runnable processes, meaning either
+%         actually running or ready to run
+%       - Total number of processes
+%       - Last created process identifier
 
 loadavg(Avg1, Avg5, Avg15, Runnables/Processes, LastPID) -->
     number(Avg1), whites,
@@ -24,7 +35,10 @@ loadavg(Avg1, Avg5, Avg15, Runnables/Processes, LastPID) -->
 %!          -RunnablesRatio, -LastPID) is det.
 %
 %   Captures and parses the current processor load average statistics on
-%   Unix systems. Does *not* work on Windows systems.
+%   Linux systems. Does *not* work on Windows systems.
+%
+%   @throws existence_error(source_sink, '/proc/loadavg') on Windows, or
+%   other operating systems that do not have a `proc` subsystem.
 
 loadavg(Avg1, Avg5, Avg15,
         RunnablesRatio, LastPID) :-
