50bf5d3a5d44977a9e01c7ff1d73be23b186622b
Roy Ratcliffe <royratcliffe@me.com>
2021-04-29 17:29:48 +0100

    Print coverage summary

--- prolog/canny/cov.pl
+++ prolog/canny/cov.pl
@@ -2,14 +2,31 @@
 :- use_module(library(canny/cover)).
 :- use_module(library(print/table)).
 
-:- initialization(up, program).
+:- initialization(cov).
 
-up :-
-    load_pack_modules(canny_tudor, Modules),
+cov :-
+    covs(Covs),
     print_table(
-        coverage_for_modules(
-            run_tests, Modules, Module, coverage{
-                                            clauses:Clauses,
-                                            cov:Cov,
-                                            fail:Fail
-                                        }), [Module, Clauses, Cov, Fail]).
+        member(
+            Module-coverage{
+                       clauses:Clauses,
+                       cov:Cov,
+                       fail:Fail
+                   }, Covs), [Module, Clauses, Cov, Fail]),
+    aggregate_all(
+        v(sum(Clauses), sum(Cov), sum(Fail), count),
+        member(_-coverage{clauses:Clauses, cov:Cov, fail:Fail}, Covs),
+        v(AllClauses, AllCov, AllFail, AllModule)),
+    AvgCov is AllCov / AllModule,
+    AvgFail is AllFail / AllModule,
+    format('Modules:~t~d~40|~n', [AllModule]),
+    format('Clauses:~t~d~40|~n', [AllClauses]),
+    format('Cov:~t~f~40|%~n', [AvgCov]),
+    format('Fail:~t~f~40|%~n', [AvgFail]).
+
+covs(Covs) :-
+    load_pack_modules(canny_tudor, Modules),
+    findall(
+        Module-Coverage,
+        coverage_for_modules(run_tests, Modules, Module, Coverage),
+        Covs).
