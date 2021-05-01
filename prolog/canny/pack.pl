fcd0030306774d4e0f118e00a12289b4db0b5d10
Roy Ratcliffe <royratcliffe@me.com>
2021-05-01 20:27:22 +0100

    Structured comments for canny_pack

--- prolog/canny/pack.pl
+++ prolog/canny/pack.pl
@@ -6,10 +6,25 @@
 :- autoload(library(prolog_pack), [pack_property/2]).
 :- use_module(library(plunit), [load_test_files/1]).
 
+%!  load_pack_modules(+Pack, -Modules) is semidet.
+%
+%   Finds and loads all Prolog module sources  for Pack. Also loads test
+%   files having once  loaded  the  pack.   Modules  becomes  a  list of
+%   successfully-loaded pack modules.
+
 load_pack_modules(Pack, Modules) :-
     pack_property(Pack, directory(Directory)),
     findall(Module, load_prolog_source(Directory, Module), Modules),
     load_test_files([]).
+
+%!  load_prolog_source(+Directory, -Module) is nondet.
+%
+%   Loads Prolog source recursively at Directory  for Module. Does *not*
+%   load non-module sources, e.g.  scripts   without  a module. Operates
+%   non-deterministically for Module. Finds and   loads  all the modules
+%   within  a  given  directory;  typically  amounts   to  a  pack  root
+%   directory. You can find the File from  which the module loaded using
+%   module properties, i.e. `module_property(Module, file(File))`.
 
 load_prolog_source(Directory, Module) :-
     directory_member(Directory, File, [file_type(prolog), recursive(true)]),
