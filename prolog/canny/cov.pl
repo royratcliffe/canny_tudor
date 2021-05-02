98bf2ae00be36fa4b18357996519476bf8772212
Roy Ratcliffe <royratcliffe@me.com>
2021-05-02 14:22:43 +0100

    Use modules for script

--- prolog/canny/cov.pl
+++ prolog/canny/cov.pl
@@ -1,6 +1,13 @@
+:- use_module(library(aggregate)).
+:- use_module(library(apply)).
+:- use_module(library(lists)).
+:- use_module(library(sort)).
+:- use_module(library(yall)).
+:- use_module(library(http/json)).
 :- use_module(library(canny/pack)).
 :- use_module(library(canny/cover)).
 :- use_module(library(print/table)).
+:- use_module(library(gh/api)).
 
 :- initialization(cov).
 
