b8b2834526407c73e49918b6668c05a26ab5cd58
Roy Ratcliffe <royratcliffe@me.com>
2022-08-15 09:15:22 +0100

    refactor: bit_fields/{3,4} (#5)
    * refactor: bit_fields/{3,4}
    
    * docs: fix bits/{4,3}
    
    * docs: more comments
    
    * chore: bump patch, log change
    
    * docs: yet more comments
    
    * docs: re-generate doc.tex
    
    * docs: xelatex

--- prolog/canny/bits.pl
+++ prolog/canny/bits.pl
@@ -1,8 +1,44 @@
-:- module(canny_bits, [bits/5, bits/4, bits/3]).
+/*  File:    canny/bits.pl
+    Author:  Roy Ratcliffe
+    Created: Aug 15 2022
+    Purpose: Canny Bits
+
+Copyright (c) 2022, Roy Ratcliffe, Northumberland, United Kingdom
+
+Permission is hereby granted, free of charge,  to any person obtaining a
+copy  of  this  software  and    associated   documentation  files  (the
+"Software"), to deal in  the   Software  without  restriction, including
+without limitation the rights to  use,   copy,  modify,  merge, publish,
+distribute, sublicense, and/or sell  copies  of   the  Software,  and to
+permit persons to whom the Software is   furnished  to do so, subject to
+the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+:- module(canny_bits,
+          [ bits/5,
+            bits/4,
+            bits/3,
+            bit_fields/3,                       % +Fields,+Shift,+Int
+            bit_fields/4                        % +Fields,+Shift,+Int0,-Int
+          ]).
 
 :- use_module(library(clpfd)).
 
 %!  bits(+Shift, +Width, ?Word, ?Bits, ?Rest) is semidet.
+%!  bits(+ShiftWidthPair, ?Word, ?Bits, ?Rest) is semidet.
+%!  bits(+ShiftWidthPair, ?Word, ?Bits) is semidet.
 %
 %   Unifies Bits within a Word using Shift  and Width. All arguments are
 %   integers treated as words of arbitrary bit-width.
@@ -28,3 +64,36 @@ bits(Shift, Width, Word, Bits, Rest) :-
 bits(Shift-Width, Word, Bits, Rest) :- bits(Shift, Width, Word, Bits, Rest).
 
 bits(Shift-Width, Word, Bits) :- bits(Shift, Width, Word, Bits, _Rest).
+
+%!  bit_fields(+Fields:list, +Shift:integer, +Int:integer) is semidet.
+%!  bit_fields(+Fields:list, +Shift:integer, +Int0:integer,
+%!  -Int:integer) is semidet.
+%
+%   Two versions of the predicate unify `Value`:`Width` bit fields with
+%   integers. The arity-3 version requires a bound Int from which to
+%   find unbound (or bound) values in the Fields; used to extract values
+%   from integers else check values semi-deterministically. The arity-4
+%   version of the predicate accumulates bit-field values by OR-wise
+%   merging shifted bits from Int0 to Int.
+%
+%   The predicates are semi-deterministic. They can fail. Failure occurs
+%   when the bit-field `Width` integers do *not* sum to Shift.
+%
+%   @arg Fields is a list of value and width terms of the form
+%   `Value:Width` where `Width` is an integer; `Value` is either a
+%   variable or an integer.
+%
+%   @arg Shift is an integer number of total bits, e.g. 8 for eight-bit
+%   bytes, 16 for sixteen-bit words and so on.
+
+bit_fields([], 0, _Int).
+bit_fields([Value:Width|Rest], Shift, Int) :-
+    Shift_ is Shift - Width,
+    Value is (Int >> Shift_) /\ ((1 << Width) - 1),
+    bit_fields(Rest, Shift_, Int).
+
+bit_fields([], 0, Int, Int).
+bit_fields([Value:Width|Rest], Shift, Int0, Int) :-
+    Shift_ is Shift - Width,
+    Int_ is Int0 \/ ((Value /\ ((1 << Width) - 1)) << Shift_),
+    bit_fields(Rest, Shift_, Int_, Int).
--- prolog/canny/bits.plt
+++ prolog/canny/bits.plt
@@ -10,4 +10,7 @@ test(bits, [true(A-B==0xf-2'1100 0011)]) :- bits(2, 4, 0xff, A, B).
 test(bits, [true(A-B==1-0)]) :- bits(31, 1, 1<<31, A, B).
 test(bits, [true(A==0x8000_0000)]) :- bits(31, 1, A, 1, 0).
 
+test(bit_fields, true(A-B == 3-3)) :- bit_fields([A:2, B:2], 4, 2'1111).
+test(bit_fields, true(A == 2'0101)) :- bit_fields([1:2, 1:2], 4, 0, A).
+
 :- end_tests(canny_bits).
