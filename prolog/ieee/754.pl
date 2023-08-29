76ab4cdb402d9ff5025f58bfb7e5cae584e98f7f
Roy Ratcliffe <roy@ratcliffe.me>
2023-08-29 13:44:12 +0100

    refactor: Word to Int

--- prolog/ieee/754.pl
+++ prolog/ieee/754.pl
@@ -29,8 +29,8 @@ inf(Width, Inf) :-
     ieee(Width, ExpWidth, _),
     Inf is ((1 << ExpWidth) - 1) << (Width - ExpWidth - 1).
 
-%!  ieee_754_float(+Width, ?Word, ?Float) is det.
-%!  ieee_754_float(-Width, ?Word, ?Float) is nondet.
+%!  ieee_754_float(+Width, ?Int, ?Float) is det.
+%!  ieee_754_float(-Width, ?Int, ?Float) is nondet.
 %
 %   Performs two-way pack and unpack for IEEE 754 floating-point numbers
 %   represented as words.
@@ -39,7 +39,7 @@ inf(Width, Inf) :-
 %   hence remains within the integer   domain.  Float arithmetic applies
 %   outside the finite-domain constraints.
 %
-%   @arg Word is a non-negative integer.   This  implementation does not
+%   @arg Int is a non-negative integer.   This  implementation does not
 %   handle negative integers. Negative support implies a non-determinate
 %   solution for packing. A positive and  negative answer exists for any
 %   given Float.
@@ -48,10 +48,10 @@ inf(Width, Inf) :-
 %   Uses Sig rather than Mantissa; Sig   short  for Significand, another
 %   word for mantissa.
 
-ieee_754_float(Width, Word, Float) :-
+ieee_754_float(Width, Int, Float) :-
     var(Float),
     !,
-    sig_exp(Width, Word, Sig, Exp),
+    sig_exp(Width, Int, Sig, Exp),
     ldexp(Sig, Float, Exp).
 ieee_754_float(Width, 0, 0.0) :- ieee(Width, _, _), !.
 ieee_754_float(Width, Inf, +1.0Inf) :- !, inf(Width, Inf).
@@ -64,28 +64,28 @@ ieee_754_float(Width, NaN, 1.5NaN) :-
     inf(Width, Inf0),
     ieee(Width, ExpWidth, _),
     NaN is Inf0 \/ (1 << (Width - ExpWidth - 2)).
-ieee_754_float(Width, Word, Float) :-
+ieee_754_float(Width, Int, Float) :-
     frexp(Float, Sig, Exp),
-    sig_exp(Width, Word, Sig * 2, Exp - 1).
+    sig_exp(Width, Int, Sig * 2, Exp - 1).
 
-sig_exp(Width, Word, Sig, Exp) :-
+sig_exp(Width, Int, Sig, Exp) :-
     ieee(Width, ExpWidth, ExpBias),
     SigWidth is Width - ExpWidth - 1,
-    bits(Width - 1, 1, Word, Sign, Word1),
-    bits(0, SigWidth, Word1, Word0, Word2),
-    bits(SigWidth, ExpWidth, Word2, Exp0, 0),
+    bits(Width - 1, 1, Int, Sign, Int1),
+    bits(0, SigWidth, Int1, Int0, Int2),
+    bits(SigWidth, ExpWidth, Int2, Exp0, 0),
     Exp #= Exp0 - ExpBias,
-    sig(Sign, Word0, 1 << SigWidth, Sig).
+    sig(Sign, Int0, 1 << SigWidth, Sig).
 
-sig(Sign, Word, Max, Sig) :- var(Sig), !, ieee_sig(Sign, Word, Max, Sig).
-sig(Sign, Word, Max, Sig) :- sig_ieee(Sign, Sig, Max, Word).
+sig(Sign, Int, Max, Sig) :- var(Sig), !, ieee_sig(Sign, Int, Max, Sig).
+sig(Sign, Int, Max, Sig) :- sig_ieee(Sign, Sig, Max, Int).
 
-ieee_sig(0, Word, Max, Sig) :- ieee_sig(Word, Max, Sig), !.
-ieee_sig(1, Word, Max, Sig) :- ieee_sig(Word, Max, Sig0), Sig is -Sig0.
+ieee_sig(0, Int, Max, Sig) :- ieee_sig(Int, Max, Sig), !.
+ieee_sig(1, Int, Max, Sig) :- ieee_sig(Int, Max, Sig0), Sig is -Sig0.
 
-ieee_sig(Word, Max, Sig) :- Sig is Word / Max + 1.
+ieee_sig(Int, Max, Sig) :- Sig is Int / Max + 1.
 
-sig_ieee(1, Sig, Max, Word) :- sign(Sig) < 0, !, sig_ieee(-Sig, Max, Word).
-sig_ieee(0, Sig, Max, Word) :- sig_ieee(Sig, Max, Word).
+sig_ieee(1, Sig, Max, Int) :- sign(Sig) < 0, !, sig_ieee(-Sig, Max, Int).
+sig_ieee(0, Sig, Max, Int) :- sig_ieee(Sig, Max, Int).
 
-sig_ieee(Sig, Max, Word) :- Word is round((Sig - 1) * Max).
+sig_ieee(Sig, Max, Int) :- Int is round((Sig - 1) * Max).
