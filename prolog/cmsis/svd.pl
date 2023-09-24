106588dc3eaa7b9f7ebb83868724ff419c655ec1
Roy Ratcliffe <roy@ratcliffe.me>
2023-09-24 14:05:12 +0100

    refactor: atom//1

--- prolog/cmsis/svd.pl
+++ prolog/cmsis/svd.pl
@@ -5,7 +5,8 @@
 */
 
 :- module(cmsis_svd,
-          [ cmsis_load_svd/2                    % +Spec,+Options
+          [ cmsis_load_svd/2,                   % +Spec,+Options
+            cmsis_c99_svd//1
           ]).
 :- autoload(library(apply), [include/3, maplist/3]).
 :- autoload(library(lists), [reverse/2]).
@@ -67,52 +68,89 @@ term(Term, Options) :-
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    Compiles CMSIS-SVD facts to C99 pre-processor hash-defines.
+
     The SVD fact base unifies with plain old atoms representing strings
     and numbers, decimal and hexadecimal.
 
+# Example Usage
+
+Suppose that the STMicroelectronics SVD facts have already been loaded
+at module `st` using the cmsis_load_svd/2 predicate. The following query
+formats C99 `#define` lines for all the STM32H747 M4 core's
+peripheral bases.
+
+    forall(
+        phrase(cmsis_c99_svd(st:device_peripheral_base('STM32H747_CM4', _, _)), Codes),
+        format('~s~n', [Codes])
+    ).
+
+#define STM32H747_CM4_COMP1_BASE 0x58003800
+#define STM32H747_CM4_CRS_BASE 0x40008400
+#define STM32H747_CM4_DAC_BASE 0x40007400
+...
+#define STM32H747_CM4_RAMECC2_BASE 0x48023000
+#define STM32H747_CM4_RAMECC3_BASE 0x58027000
+#define STM32H747_CM4_ART_BASE 0x40024400
+
+The order matches the SVD ordering. The following does the same thing
+after sorting by the base address ascending.
+
+    A = 'STM32H747_CM4',
+    findall(t(A, B, C), st:device_peripheral_baseAddress(A, B, C), D),
+    sort(3, @=<, D, E),
+    forall(member(t(A, B, C), E),
+           (   phrase(cmsis_c99_svd(st:device_peripheral_base(A, B, C)), Codes),
+               format('~s~n', [Codes])
+           )).
+
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-cmsis_c99_svd(M:device_peripheral_base(A, B, C)) -->
-    { M:device_peripheral_baseAddress(A, B, C),
-      atom_codes(A, A_),
-      atom_codes(B, B_),
-      atom_codes(C, C_)
-    },
-    "#define ", A_, "_", B_, "_BASE ", C_, "".
-cmsis_c99_svd(M:device_peripheral_register_offset(A, B, C, D)) -->
-    { M:device_peripheral_register_addressOffset(A, B, C, D),
-      atom_codes(A, A_),
-      atom_codes(B, B_),
-      atom_codes(C, C_),
-      atom_codes(D, D_)
-    },
-    "#define ", A_, "_", B_, "_", C_, "_OFFSET ", D_, "".
-cmsis_c99_svd(M:device_peripheral_register(A, B, C)) -->
-    { M:device_peripheral_baseAddress(A, B, _),
-      M:device_peripheral_register_addressOffset(A, B, C, _),
-      atom_codes(A, A_),
-      atom_codes(B, B_),
-      atom_codes(C, C_)
-    },
-    "#define ", A_, "_", B_, "_", C_, " (",
-    A_, "_", B_, "_BASE + ",
-    A_, "_", B_, "_", C_, "_OFFSET)".
-cmsis_c99_svd(M:device_peripheral_register_field(A, B, C, D)) -->
-    { M:device_peripheral_register_field_bitOffset(A, B, C, D, Offset),
-      M:device_peripheral_register_field_bitWidth(A, B, C, D, Width),
-      atom_codes(A, A_),
-      /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+%!  cmsis_c99_svd(M:Term) is nondet.
 
-      No need to express B because C_ incorporates it.
-
-      atom_codes(B, B_),
-
-      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-      atom_codes(C, C_),
-      atom_codes(D, D_),
-      atom_codes(Offset, Offset_),
-      atom_codes(Width, Width_)
+cmsis_c99_svd(M:device_peripheral_base(Device, Peripheral, BaseAddress)) -->
+    { M:device_peripheral_baseAddress(Device, Peripheral, BaseAddress)
     },
-    "#define ", A_, "_", C_, "_", D_, " (((1 << ",
-    Width_, ") - 1) << ",
-    Offset_, ")".
+    "#define ",
+    atom(Device), "_", atom(Peripheral), "_BASE ",
+    atom(BaseAddress).
+cmsis_c99_svd(M:device_peripheral_register_offset(Device, Peripheral, Register, D)) -->
+    { M:device_peripheral_register_addressOffset(Device, Peripheral, Register, D)
+    },
+    "#define ",
+    atom(Device), "_", atom(Peripheral), "_", atom(Register), "_OFFSET ",
+    atom(D).
+cmsis_c99_svd(M:device_peripheral_register(Device, Peripheral, Register)) -->
+    { M:device_peripheral_baseAddress(Device, Peripheral, _),
+      M:device_peripheral_register_addressOffset(Device, Peripheral, Register, _)
+    },
+    "#define ",
+    atom(Device), "_", atom(Peripheral), "_", atom(Register), " (",
+    atom(Device), "_", atom(Peripheral), "_BASE + ",
+    atom(Device), "_", atom(Peripheral), "_", atom(Register), "_OFFSET)".
+cmsis_c99_svd(M:device_peripheral_register_field(Device, Peripheral, Register, Field)) -->
+    { M:device_peripheral_register_field_bitOffset(
+            Device, Peripheral, Register, Field, Offset),
+      M:device_peripheral_register_field_bitWidth(
+            Device, Peripheral, Register, Field, Width)
+    },
+    "#define ",
+    atom(Device), "_",
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    No need to express the Peripheral if the Register already incorporates
+    it. Doing so would unnecessarily double up on the name of the manifest
+    constant.
+    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    (   { atom_prefix(Register, Peripheral)
+        }
+    ->  []
+    ;   atom(Peripheral), "_"
+    ),
+    atom(Register), "_", atom(Field), " (((1 << ",
+    atom(Width), ") - 1) << ",
+    atom(Offset), ")".
+
+atom(Atom) -->
+    { atom_codes(Atom, Codes)
+    },
+    Codes.
