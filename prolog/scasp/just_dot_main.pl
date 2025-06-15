216879fea40a4ef06d7eac62f76df5ba2271a216
Roy Ratcliffe <roy@ratcliffe.me>
2025-06-15 18:35:58 +0100

    fix: update option types and help descriptions in just_dot_main.pl

--- prolog/scasp/just_dot_main.pl
+++ prolog/scasp/just_dot_main.pl
@@ -3,9 +3,20 @@
 
 :- initialization(main, main).
 
-opt_type(t, tab, nonneg).
-opt_type(r, rankdir, atom).
-opt_type(e, elides, term).
+opt_type(tab, tab, nonneg).
+opt_type(rankdir, rankdir, atom).
+opt_type(bgcolor, bgcolor, atom).
+opt_type(node, node, term).
+opt_type(edge, edge, term).
+opt_type(elides, elides, term).
+
+opt_help(help(header), 'Generate a DOT file from a JSON file produced by scasp').
+opt_help(tab, 'Tab size for indentation').
+opt_help(rankdir, 'Direction of the graph layout').
+opt_help(bgcolor, 'Background color of the graph').
+opt_help(node, 'Node attributes').
+opt_help(edge, 'Edge attributes').
+opt_help(elides, 'Nodes to elide in the graph').
 
 main(Argv) :-
     argv_options(Argv, Positional, Options, []),
