1b7474f9177d2b8cb84e93a029f8614172b7edce
Roy Ratcliffe <roy@ratcliffe.me>
2025-06-17 06:33:49 +0100

    docs: update header information in scasp_just_dot.pl to include purpose, license, version, and status

--- prolog/scasp/just_dot.pl
+++ prolog/scasp/just_dot.pl
@@ -1,7 +1,33 @@
 /*  File:    scasp_just_dot.pl
     Author:  Roy Ratcliffe
     Created: Jun  8 2025
-    Purpose:
+    Purpose: Generate a DOT graph from a JSON source produced by s(CASP)
+    License: MIT License
+    Version: 1.0.0
+    Status:  Stable
+    Updated: 2025-06-08
+
+Copyright (c) 2025, Roy Ratcliffe, Northumberland, United Kingdom
+
+Permission is hereby granted, free of charge,  to any person obtaining a
+copy  of  this  software  and    associated   documentation  files  (the
+"Software"), to deal in  the   Software  without  restriction, including
+without limitation the rights to  use,   copy,  modify,  merge, publish,
+distribute, sub-license, and/or sell copies  of   the  Software,  and to
+permit persons to whom the Software is   furnished  to do so, subject to
+the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 */
 
 :- module(scasp_just_dot,
