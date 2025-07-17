f9cb5b05f57c6e32c13024f5fea0dd9a18f61d38
Roy Ratcliffe <roy@ratcliffe.me>
2025-07-17 13:46:16 +0100

    docs: update LaTeX compilation instructions and ensure PDF is opened in browser

--- man/canny_tudor.R
+++ man/canny_tudor.R
@@ -3,5 +3,9 @@
 # `tinytex` package is installed and configured in your R environment. This
 # command is typically run in an R script or console. Make sure to have the
 # `canny_tudor.tex` file in the working directory before running this command.
-# You can run this command in R to compile the LaTeX document.
-tinytex::xelatex("canny_tudor.tex")
+# You can run this command in R to compile the LaTeX document. Open the
+# generated PDF in a web browser using the `browseURL` function from the `utils`
+# package. This will allow you to view the PDF output directly in your default
+# web browser. Ensures that the PDF file is generated successfully before
+# running this command.
+utils::browseURL(tinytex::xelatex("canny_tudor.tex"))
