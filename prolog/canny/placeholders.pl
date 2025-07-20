69c2eb998eae32681cdeb985e99e5ff7ccabf47c
Roy Ratcliffe <roy@ratcliffe.me>
2025-07-20 08:18:51 +0100

    docs: enhance module documentation for formatting placeholders

--- prolog/canny/placeholders.pl
+++ prolog/canny/placeholders.pl
@@ -37,6 +37,31 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 :- use_module(library(option), [select_option/3]).
 :- use_module(library(dcg/basics), [string_without/4]).
 
+/** <module> Formatting Placeholders
+
+This module provides predicates for formatting strings with placeholders.
+Placeholders are specified in the form of `{name}` within a format string.
+The placeholders are replaced with corresponding values from a list of
+options, where each option is specified as `name(Value)`.
+The result is an atom containing the formatted string.
+The module uses DCG rules to parse the format string and replace the
+placeholders with the corresponding values.
+
+The main predicate is `format_placeholders/3`, which takes a format
+string, an atom to hold the result, and a list of options. It processes
+the format string, replacing placeholders with their corresponding values
+from the options list. If a placeholder does not have a corresponding value,
+it will fail.
+
+The `format_placeholders/3` predicate formats a string with placeholders, while
+`format_placeholders/4` allows for additional options to be returned; namely,
+the remaining options after processing the placeholders.
+
+@author Roy Ratcliffe
+@version 0.1.0
+
+*/
+
 %!  format_placeholders(+Format, -Atom, +Options) is det.
 %!  format_placeholders(+Format, -Atom, +Options, -RestOptions) is det.
 %
