529c1e5588f34b0cb7a017ae3bd504ac719c5153
Roy Ratcliffe <roy@ratcliffe.me>
2025-08-12 17:21:21 +0100

    Add xdigit_weights_and_bytes predicate for hexadecimal to byte conversion

--- prolog/canny/bits.pl
+++ prolog/canny/bits.pl
@@ -120,3 +120,40 @@ rbit(Shift, Int0, Reverse0, Int, Reverse) :-
     Reverse_ is (Reverse0 << 1) \/ (Int0 /\ 1),
     Int_ is Int0 >> 1,
     rbit(Shift_, Int_, Reverse_, Int, Reverse).
+
+%!  xdigit_weights_and_bytes(?Weights:list(integer), ?Bytes:list(integer)) is semidet.
+%
+%   Convert a list of hexadecimal digit weights   to a list of bytes, or
+%   vice  versa.  Uses  CLP(FD)  constraints  to  ensure  valid  two-way
+%   conversions.
+%
+%   Examples:
+%
+%       ?- xdigit_weights_and_bytes([10, 11, 12, 13], Bytes).
+%       Bytes = [171, 205].
+%
+%       ?- xdigit_weights_and_bytes(Weights, [171, 205]).
+%       Weights = [10, 11, 12, 13].
+%
+%   This predicate is semidet, meaning it  can succeed or fail depending
+%   on the input. The elements of the Weights  list must be in the range
+%   0..15, and the elements of  the  Bytes   list  must  be in the range
+%   0..255. The length of the Weights and   Bytes lists must differ by a
+%   factor of two for the conversion   to succeed. Two hexadecimal digit
+%   weights correspond to one byte.
+%
+%   @arg Weights A list of hexadecimal digit weights (0-15).
+%   @arg Bytes A list of bytes (0-255).
+
+xdigit_weights_and_bytes([], []).
+xdigit_weights_and_bytes([H1, H2|T0], [H|T]) :-
+    H #= (H1 << 4) \/ H2,
+    % Perform the inverse operations; convert back from a byte to weights.
+    % Extract the high and low nibbles.
+    H1 #= H >> 4,
+    H2 #= H /\ 16'f,
+    % Ensure that the weights are valid for hexadecimal digits.
+    % H1 and H2 must be in the range 0..15 otherwise the conversion fails.
+    H1 in 16'0..16'f,
+    H2 in 16'0..16'f,
+    xdigit_weights_and_bytes(T0, T).
