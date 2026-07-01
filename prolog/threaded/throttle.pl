37f11470923908d8c454ec9358726d49167efef7
Roy Ratcliffe <roy@ratcliffe.me>
2026-07-01 14:10:56 +0100

    Update copyright notice and module documentation in throttle.pl
    Revise the copyright section to include author details and
    permissions. Enhance module documentation to clarify the
    purpose of the threaded throttle mechanism for Redis streams.

--- prolog/threaded/throttle.pl
+++ prolog/threaded/throttle.pl
@@ -1,3 +1,31 @@
+/*  File:    threaded/throttle.pl
+    Author:  Roy Ratcliffe
+    Created: Jun  7 2026
+    Purpose: Threaded throttle mechanism for Redis streams
+
+Copyright (c) 2026, Roy Ratcliffe, Northumberland, United Kingdom
+
+Permission is hereby granted, free of charge,  to any person obtaining a
+copy  of  this  software  and    associated   documentation  files  (the
+"Software"), to deal in  the   Software  without  restriction, including
+without limitation the rights to  use,   copy,  modify,  merge, publish,
+distribute, sub-license, and/or sell copies  of   the  Software,  and to
+permit persons to whom the Software is   furnished  to do so, subject to
+the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 :- module(threaded_throttle,
           [ consume_threaded_throttle/2 % +Key, +Field
           , unconsume_threaded_throttle/2 % +Key, +Field
